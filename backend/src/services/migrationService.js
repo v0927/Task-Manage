6fd3d53bfa6da57b04b966e1e5b2dd9c39bdc4ee
Valentina Herrera <vahegiraldo@gmail.com>
2026-03-14 16:43:18 -0400

    Fix migration service path

--- backend/src/services/migrationService.js
+++ backend/src/services/migrationService.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const path = require('path');
-const pool = require('./config/database');
+const pool = require('../config/database');
 
 async function runMigrations() {
   try {
